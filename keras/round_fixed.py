786561aa97ce91bd19bf397f87c2cd462b009559
Joey Li <joey.yiwei.li@huawei.com>
2019-04-10 14:08:51 -0400

    use in_train_phase to save time on training.

--- keras/round_fixed.py
+++ keras/round_fixed.py
@@ -23,20 +23,25 @@ class RoundToFixed(Layer):
         # using the statement below made the train hang for some reason
         # return K.map_fn(lambda i : K.map_fn(lambda j : K.variable(float(FXnum(np.asscalar(j.numpy())))), i), x)
         #print("x: " + str(x))
-        x_numpy = x.numpy()
-        x_rounded = np.zeros(x_numpy.shape)
-        
-        fam = FXfamily(64)
-        # TODO: handle arbitrary dimensions
-        for i in range(x_numpy.shape[0]):
-            for j in range(x_numpy.shape[1]):
-                val = x_numpy[i][j]
-                x_rounded[i][j] = float(FXnum(np.asscalar(val)))
+        if K.in_train_phase(True, False):
+            return x
+        else:
 
-        x_rounded = tf.convert_to_tensor(x_rounded, dtype=tf.float32)
-        #print("x_rounded: " + str(x_rounded))
-        return x_rounded
+            x_numpy = x.numpy()
+            x_rounded = np.zeros(x_numpy.shape)
 
+            fam = FXfamily(64)
+            # TODO: handle arbitrary dimensions
+            for i in range(x_numpy.shape[0]):
+                for j in range(x_numpy.shape[1]):
+                    val = x_numpy[i][j]
+                    x_rounded[i][j] = float(FXnum(np.asscalar(val)))
+
+            x_rounded = tf.convert_to_tensor(x_rounded, dtype=tf.float32)
+
+            #print("x_rounded: " + str(x_rounded))
+            # return x_rounded
+            return x_rounded
 
     def compute_output_shape(self, input_shape):
         # this is an elementwise op so output shape is same as input shape
