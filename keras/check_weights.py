1b02c2efdbf37dd5e97c3f19ec37100315ccf515
Mostafa Elhoushi <mostafa.elhoushi@huawei.com>
2019-04-11 13:20:02 -0400

    shorten file renaming

--- keras/check_weights.py
+++ keras/check_weights.py
@@ -15,6 +15,10 @@ import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
+#model = load_model("./saved_models.shift_last_stack_only/cifar10_ResNet20v1_model.144.h5", custom_objects={'Conv2DShift': Conv2DShift, 'DenseShift': DenseShift})
+model = resnet_v1(input_shape=(32,32,3), depth=20, shift_depth=56)
+model.load_weights("./saved_models/cifar10_ResNet20v1_model_shift_22/model.003.h5")
+
 model_dir = "./saved_models/cifar10_ResNet50v1_model_shift_10/"
 model_file = "model.164.h5"
 
