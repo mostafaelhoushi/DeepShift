3321512bd02cbcc3974a604690b8393757127ec9
Mostafa Elhoushi <mostafa.elhoushi@huawei.com>
2019-04-11 17:13:21 -0400

    get conv and resnet to work in eager execution mode

--- keras/convolutional_shift.py
+++ keras/convolutional_shift.py
@@ -3,7 +3,7 @@ from tensorflow.python.keras.layers.convolutional import *
 
 from shift_layer import *
 
-class ConvShift(Conv):
+class ConvShift(tf.keras.layers.Layer):
   def __init__(self, rank,
                filters,
                kernel_size,
@@ -23,34 +23,86 @@ class ConvShift(Conv):
                trainable=True,
                name=None,
                **kwargs):
-    super(ConvShift, self).__init__(rank=rank,
-                                    filters=filters,
-                                    kernel_size=kernel_size,
-                                    strides=strides,
-                                    padding=padding,
-                                    data_format=data_format,
-                                    dilation_rate=dilation_rate,
-                                    activation=activation,
-                                    use_bias=use_bias,
-                                    kernel_initializer=kernel_initializer,
-                                    bias_initializer=bias_initializer,
-                                    kernel_regularizer=kernel_regularizer,
-                                    bias_regularizer=bias_regularizer,
-                                    activity_regularizer=activity_regularizer,
-                                    kernel_constraint=kernel_constraint,
-                                    bias_constraint=bias_constraint,
-                                    trainable=trainable,
-                                    name=name,
-                                    **kwargs)
+    super(ConvShift, self).__init__(
+        trainable=trainable,
+        name=name,
+        activity_regularizer=regularizers.get(activity_regularizer),
+        **kwargs)
+    self.rank = rank
+    self.filters = filters
+    self.kernel_size = conv_utils.normalize_tuple(
+        kernel_size, rank, 'kernel_size')
+    self.strides = conv_utils.normalize_tuple(strides, rank, 'strides')
+    self.padding = conv_utils.normalize_padding(padding)
+    if (self.padding == 'causal' and not isinstance(self,
+                                                    (Conv1D, SeparableConv1D))):
+      raise ValueError('Causal padding is only supported for `Conv1D`'
+                       'and ``SeparableConv1D`.')
+    self.data_format = conv_utils.normalize_data_format(data_format)
+    self.dilation_rate = conv_utils.normalize_tuple(
+        dilation_rate, rank, 'dilation_rate')
+    self.activation = activations.get(activation)
+    self.use_bias = use_bias
+    self.kernel_initializer = initializers.get(kernel_initializer)
+    self.bias_initializer = initializers.get(bias_initializer)
+    self.kernel_regularizer = regularizers.get(kernel_regularizer)
+    self.bias_regularizer = regularizers.get(bias_regularizer)
+    self.kernel_constraint = constraints.get(kernel_constraint)
+    self.bias_constraint = constraints.get(bias_constraint)
+    self.input_spec = InputSpec(ndim=self.rank + 2)
 
   def build(self, input_shape):
     # TODO: minimize redundancy by invoking build of Conv
     # and do minimal additions here
-    super(ConvShift, self).build(input_shape)
+    input_shape = tensor_shape.TensorShape(input_shape)
+    if self.data_format == 'channels_first':
+      channel_axis = 1
+    else:
+      channel_axis = -1
+    if input_shape.dims[channel_axis].value is None:
+      raise ValueError('The channel dimension of the inputs '
+                       'should be defined. Found `None`.')
+    input_dim = int(input_shape[channel_axis])
+    kernel_shape = self.kernel_size + (input_dim, self.filters)
 
-    kernel_shape = self.kernel.shape
+    #TODO: rename "kernel" to "shift"
+    self.kernel = self.add_weight(
+        name='kernel',
+        shape=kernel_shape,
+        initializer=self.kernel_initializer,
+        regularizer=self.kernel_regularizer,
+        constraint=self.kernel_constraint,
+        trainable=True,
+        dtype=self.dtype)
+    if self.use_bias:
+      self.bias = self.add_weight(
+          name='bias',
+          shape=(self.filters,),
+          initializer=self.bias_initializer,
+          regularizer=self.bias_regularizer,
+          constraint=self.bias_constraint,
+          trainable=True,
+          dtype=self.dtype)
+    else:
+      self.bias = None
+    self.input_spec = InputSpec(ndim=self.rank + 2,
+                                axes={channel_axis: input_dim})
+    if self.padding == 'causal':
+      op_padding = 'valid'
+    else:
+      op_padding = self.padding
+    if not isinstance(op_padding, (list, tuple)):
+      op_padding = op_padding.upper()
+    self._convolution_op = nn_ops.Convolution(
+        input_shape,
+        filter_shape=self.kernel.shape,
+        dilation_rate=self.dilation_rate,
+        strides=self.strides,
+        padding=op_padding,
+        data_format=conv_utils.convert_data_format(self.data_format,
+                                                   self.rank + 2))
+    self.built = True
 
-    #TODO: rename "self.kernel" to "self.shift" ?
     self.sign = self.add_weight(shape=kernel_shape,
                                     initializer=RoundedRandomUniform(0,1),
                                     name='sign',
@@ -150,7 +202,7 @@ class Conv2DShift(ConvShift):
 
     def inference_fun(self, x):
         # TODO: implement bitwise
-        return super(Conv2DShift, self).call(x)
+        return None
 
     def call(self, inputs):
         if K.in_train_phase(True, False):
@@ -158,79 +210,6 @@ class Conv2DShift(ConvShift):
         else:
             return self.inference_fun(x)
 
-        if self.rank == 1:
-            outputs = K.conv1d(
-                inputs,
-                W,
-                strides=self.strides[0],
-                padding=self.padding,
-                data_format=self.data_format,
-                dilation_rate=self.dilation_rate[0])
-        if self.rank == 2:
-            outputs = K.conv2d(
-                inputs,
-                W,
-                strides=self.strides,
-                padding=self.padding,
-                data_format=self.data_format,
-                dilation_rate=self.dilation_rate)
-        if self.rank == 3:
-            outputs = K.conv3d(
-                inputs,
-                W,
-                strides=self.strides,
-                padding=self.padding,
-                data_format=self.data_format,
-                dilation_rate=self.dilation_rate)
+# Aliases
 
-        if self.use_bias:
-            outputs = K.bias_add(
-                outputs,
-                self.bias,
-                data_format=self.data_format)
-
-        if self.activation is not None:
-            return self.activation(outputs)
-        return outputs
-
-class Conv2DShift(_ConvShift):
-    @interfaces.legacy_conv2d_support
-    def __init__(self, filters,
-                 kernel_size,
-                 strides=(1, 1),
-                 padding='valid',
-                 data_format=None,
-                 dilation_rate=(1, 1),
-                 activation=None,
-                 use_bias=True,
-                 kernel_initializer=RoundedRandomUniform(),
-                 bias_initializer='zeros',
-                 kernel_regularizer=None,
-                 bias_regularizer=None,
-                 activity_regularizer=None,
-                 kernel_constraint=IntegerConstraint(),
-                 bias_constraint=None,
-                 **kwargs):
-        super(Conv2DShift, self).__init__(
-            rank=2,
-            filters=filters,
-            kernel_size=kernel_size,
-            strides=strides,
-            padding=padding,
-            data_format=data_format,
-            dilation_rate=dilation_rate,
-            activation=activation,
-            use_bias=use_bias,
-            kernel_initializer=kernel_initializer,
-            bias_initializer=bias_initializer,
-            kernel_regularizer=kernel_regularizer,
-            bias_regularizer=bias_regularizer,
-            activity_regularizer=activity_regularizer,
-            kernel_constraint=kernel_constraint,
-            bias_constraint=bias_constraint,
-            **kwargs)
-
-    def get_config(self):
-        config = super(Conv2DShift, self).get_config()
-        config.pop('rank')
-        return config
+Convolution2DShift = Conv2DShift
