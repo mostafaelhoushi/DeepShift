8d6b04c64ee5fbc3d145724022cd14f88cb3eb2f
Mostafa Elhoushi <mostafa.elhoushi@huawei.com>
2019-04-19 08:48:14 -0400

    return original code - because non-seq code is producing bad val acc - and add option for shift_depth

--- keras/convert_to_shift.py
+++ keras/convert_to_shift.py
@@ -5,6 +5,68 @@ from tensorflow.keras.models import Model
 from shift_layer import DenseShift
 from convolutional_shift import Conv2DShift
 
+def convert_to_shift(model, num_layers = -1, num_to_replace=None):
+    # create input layer for new model
+    input_shape = model.input.shape[1:] # first shape element is batch size so don't copy it
+    inputs = tf.keras.Input(shape=input_shape)
+
+    # copy the layers - except the input layer - from original model
+    layers = [l for l in model.layers[1:]]
+
+    # count number of instances to keep
+    num_layer_type = sum(1 for l in layers if type(l)==Conv2D)
+    if (num_to_replace is not None):
+        num_layer_keep = max(num_layer_type - num_to_replace, 0)
+    else:
+        num_layer_keep = num_layer_type
+
+    num_layer_kept = 0
+    x = inputs
+    for i, layer in enumerate(layers):
+        if type(layer) == Dense and num_to_replace is not None:
+            input = layer.input
+            output = layer.output
+            weights = layer.weights
+
+            # weights of Dense has shape: (features_in, features_out)
+            _, features_out = weights[0].shape
+            #TODO: copy all other attributes. Consider using get_config() and from_config()
+            dense_shift_layer = DenseShift(weights[0].shape[-1]) 
+
+            x = dense_shift_layer(x)
+
+        elif type(layer) == Conv2D:
+            num_layer_kept += 1
+            if num_layer_kept > num_layer_keep: 
+                input = layer.input
+                output = layer.output
+                weights = layer.weights
+
+                # weights of Conv2D has shape: (filter_height, filter_width, channels_in, channels_out)
+                filter_height, filter_width, _, channels_out = weights[0].shape.as_list()
+                #TODO: copy all other attributes. Consider using get_config() and from_config()
+                conv2d_shift_layer = Conv2DShift(filters=channels_out, kernel_size = (filter_height, filter_width)) 
+
+                x = conv2d_shift_layer(x)
+            else:
+                x = layer(x)
+
+        else:
+            x = layer(x)
+    outputs = x
+
+    model_converted = Model(inputs=inputs, outputs=outputs)
+    #TODO: Copy other attributes such as learning rate, optimizer, etc.
+    return model_converted
+
+'''
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, Dense
+from tensorflow.keras.models import Model
+
+from shift_layer import DenseShift
+from convolutional_shift import Conv2DShift
+
 # Source: https://stackoverflow.com/a/54517478/3880948
 def insert_layer(model, layer_type, insert_layer_factory,
                  insert_layer_name=None, position='replace', num_to_replace=None):
@@ -104,3 +166,4 @@ def convert_to_shift(model, num_to_replace = None):
 
     #TODO: Copy other attributes such as learning rate, optimizer, etc.
     return model_converted
+'''
