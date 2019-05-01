97d1461d867cab2728deb52b414afcacdbc0a614
Mostafa Elhoushi <mostafa.elhoushi@huawei.com>
2019-05-01 13:17:07 -0400

    use autograd

--- pytorch/shift.py
+++ pytorch/shift.py
@@ -1,47 +1,9 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Function
 
 import math
-
-# Inherit from Function
-class LinearShiftFunction(Function):
-
-    # Note that both forward and backward are @staticmethods
-    @staticmethod
-    # bias is an optional argument
-    def forward(ctx, input, weight, bias=None):
-        ctx.save_for_backward(input, weight, bias)
-        output = input.mm(2**weight.t())
-        if bias is not None:
-            output += bias.unsqueeze(0).expand_as(output)
-        return output
-
-    # This function has only a single output, so it gets only one gradient
-    @staticmethod
-    def backward(ctx, grad_output):
-        # This is a pattern that is very convenient - at the top of backward
-        # unpack saved_tensors and initialize all gradients w.r.t. inputs to
-        # None. Thanks to the fact that additional trailing Nones are
-        # ignored, the return statement is simple even when the function has
-        # optional inputs.
-        input, weight, bias = ctx.saved_tensors
-        grad_input = grad_weight = grad_bias = None
-
-        # These needs_input_grad checks are optional and there only to
-        # improve efficiency. If you want to make your code simpler, you can
-        # skip them. Returning gradients for inputs that don't require it is
-        # not an error.
-        if ctx.needs_input_grad[0]:
-            grad_input = math.log(2) * grad_output.mm(2**weight)
-        if ctx.needs_input_grad[1]:
-            grad_weight = grad_output.t().mm(input)
-        if bias is not None and ctx.needs_input_grad[2]:
-            grad_bias = grad_output.sum(0).squeeze(0)
-
-        return grad_input, grad_weight, grad_bias
-
-linear_shift = LinearShiftFunction.apply
 
 class LinearShift(nn.Module):
     def __init__(self, input_features, output_features, bias=True):
@@ -71,7 +33,7 @@ class LinearShift(nn.Module):
 
     def forward(self, input):
         # See the autograd section for explanation of what happens here.
-        return LinearShiftFunction.apply(input, self.weight, self.bias)
+        return F.linear(input, 2**self.weight, self.bias)
 
     def extra_repr(self):
         # (Optional)Set the extra information about this module. You can test
