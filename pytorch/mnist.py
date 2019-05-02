85a83ff3e9abc2e9f580099aa0a2630d58d6bcc9
Mostafa Elhoushi <mostafa.elhoushi@huawei.com>
2019-05-02 14:15:02 -0400

    make mnist fc only, add dropout and loss type

--- pytorch/mnist.py
+++ pytorch/mnist.py
@@ -11,28 +11,28 @@ import shift
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)
-        self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(4*4*50, 500)
-        self.fc2 = shift.LinearShift(500, 10)
+        self.fc1 = nn.Linear(1*28*28, 512) # shift.LinearShift(1*28*28, 512) 
+        self.dropout1 = nn.Dropout(0.2)
+        self.fc2 = shift.LinearShift(512, 512)
+        self.dropout2 = nn.Dropout(0.2)
+        self.fc3 = shift.LinearShift(512, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4*4*50)
+        x = x.view(-1, 1 * 28 * 28) 
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.dropout1(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout2(x)
+        x = self.fc3(x)
         return F.log_softmax(x, dim=1)
     
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(args, model, device, train_loader, loss_fn, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        loss = loss_fn(output, target)
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
@@ -40,7 +40,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
 
-def test(args, model, device, test_loader):
+def test(args, model, device, test_loader, loss_fn):
     model.eval()
     test_loss = 0
     correct = 0
@@ -48,7 +48,7 @@ def test(args, model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            test_loss += loss_fn(output, target, reduction='sum').item() # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
@@ -104,14 +104,15 @@ def main():
 
 
     model = Net().to(device)
+    loss_fn = F.nll_loss
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader)
+        train(args, model, device, train_loader, loss_fn, optimizer, epoch)
+        test(args, model, device, test_loader, loss_fn)
 
     if (args.save_model):
-        torch.save(model.state_dict(),"mnist_cnn.pt")
+        torch.save(model.state_dict(),"mnist_fc.pt")
         
 if __name__ == '__main__':
     main()
