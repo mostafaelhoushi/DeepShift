b802218760e729e320c43869b57d6ae89998d6e3
Mostafa Elhoushi <mostafa.elhoushi@huawei.com>
2019-05-03 13:08:10 -0400

    look for linear layer and replace it

--- pytorch/convert_to_shift.py
+++ pytorch/convert_to_shift.py
@@ -1,7 +1,11 @@
 import torch
+import torch.nn as nn
+
 import shift
 
 def convert_to_shift(model, shift_depth):
-    model._modules['fc1'] = shift.LinearShift(1*28*28, 512) 
+    for name, module in model.named_modules():
+        if type(module) == nn.Linear:
+            model._modules[name] = shift.LinearShift(1*28*28, 512) 
 
     return model
