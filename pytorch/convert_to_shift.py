9cdd85ddd22859d95426e2183e76e60240ac5413
Mostafa Elhoushi <mostafa.elhoushi@huawei.com>
2019-05-03 15:25:10 -0400

    loop layers in reverse order

--- pytorch/convert_to_shift.py
+++ pytorch/convert_to_shift.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import shift
 
 def convert_to_shift(model, shift_depth):
-    for name, module in model.named_modules():
+    for name, module in reversed(list(model.named_modules())):
         if type(module) == nn.Linear:
             model._modules[name] = shift.LinearShift(module.in_features, module.out_features, module.bias is not None) 
 
