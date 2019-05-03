6d1fa4869b4e6f76941458a44f922b7e0c16b344
Mostafa Elhoushi <mostafa.elhoushi@huawei.com>
2019-05-03 15:18:26 -0400

    handle arbitrary features size during replacement

--- pytorch/convert_to_shift.py
+++ pytorch/convert_to_shift.py
@@ -6,6 +6,6 @@ import shift
 def convert_to_shift(model, shift_depth):
     for name, module in model.named_modules():
         if type(module) == nn.Linear:
-            model._modules[name] = shift.LinearShift(1*28*28, 512) 
+            model._modules[name] = shift.LinearShift(module.in_features, module.out_features, module.bias is not None) 
 
     return model
